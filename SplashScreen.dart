845b304913eadd20858b9e384790e9ebf5669080
abdul saboor <79809633+AbdulSaboor16@users.noreply.github.com>
2022-12-21 05:30:39 -0800

    initial commit

---
+++ SplashScreen.dart
@@ -0,0 +1,35 @@
+import 'dart:async';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:pratice/homepage.dart';
+
+class Screen extends StatefulWidget {
+  const Screen({super.key});
+
+  @override
+  State<Screen> createState() => _ScreenState();
+}
+
+class _ScreenState extends State<Screen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    Timer(
+      Duration(seconds: 3),
+      ()=> Navigator.push(
+        context,
+      MaterialPageRoute(builder: (context) => const Home()),
+      ));
+  }
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      body: Center(
+        child: Image.asset('assets/images/splash.png'), ),
+    );
+  }
+}
